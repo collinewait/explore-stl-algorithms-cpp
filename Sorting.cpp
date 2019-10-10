9eea98e414e02fd52e10db5138d14082b8b0e0cf
collinewait <collinewait17@gmail.com>
2019-10-10 21:17:38 +0300

    use nth_element

--- Sorting.cpp
+++ Sorting.cpp
@@ -75,6 +75,13 @@ int main()
 
     vector<int> v3(3);
     partial_sort_copy(begin(v), end(v), begin(v3), end(v3));
+
+    v2 = { 1, 5, 4, 2, 9, 7, 3, 8, 2 };
+    int i = *(begin(v2) + 4);
+    cout << "i: " << i << endl;
+    nth_element(begin(v2), begin(v2)+4, end(v2));
+    i = *(begin(v2) + 4);
+    cout << "i again: " << i << endl;
     
     return 0;
 }
